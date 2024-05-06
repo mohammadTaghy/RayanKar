6b92a38873b955f36e9a8c3326c6d3d18a2294f6
Taghy Yami <m.taghy.yami@gmail.com>
2024-05-06 19:18:50 +0330

    Add CustomerListQuery_test and pass

---
+++ Test/Application.Test/UseCases/Customers/Query/Customers/CustomerItemListQyeryHandler_Test.cs
@@ -0,0 +1,139 @@
+﻿using Application.IRepositoryRead;
+using Application.UseCases.Customers.Query;
+using Application.UseCases.Customers.Query.Customers;
+using Domain.ReadEntitis;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.OData.Extensions;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using Microsoft.OData.UriParser;
+using System.Linq.Expressions;
+using System.Net.Http;
+
+
+namespace Application.Test.UseCases.Customers.Query.Customers
+{
+    public class CustomerItemListQyeryHandler_Test : QueryUseCaseTestBase<CustomerRead, ICustomerReadRepository>
+    {
+        private readonly CustomerItemListQyeryHandler _handler;
+
+
+        public CustomerItemListQyeryHandler_Test(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            _handler = new CustomerItemListQyeryHandler(_readRepoMock.Object, _mapper.Object);
+
+        }
+        [Fact]
+        public void CustomerItemQyeryHandler_GivenNullParameter_ArgumentNullException()
+        {
+            Assert.ThrowsAsync<ArgumentNullException>(() => _handler.Handle(null, CancellationToken.None));
+
+            _readRepoMock.Verify(p => p.ItemList(It.IsAny<ODataQueryOptions<CustomerRead>>()), Times.Never);
+        }
+        [Fact]
+        public async Task UserListQueryHandler_NotErrorWhenTopLessThanZero_QueryTest()
+        {
+            _readRepoMock.Setup(p => p.ItemList(It.IsAny<ODataQueryOptions<CustomerRead>>()))
+                .Returns(Task.FromResult(new Tuple<List<CustomerRead>, int>(new List<CustomerRead>(), 0)));
+
+            QueryResponse<List<CustomerDto>> result =
+                await _handler.Handle(
+                    GetCustomerListQuery("https://localhost:8080/?$FirstName='qqqq'$top=10&$skip=0&$orderby=Id"),
+                    CancellationToken.None);
+
+            _readRepoMock.Verify(p => p.ItemList(It.IsAny<ODataQueryOptions<CustomerRead>>()), Times.Once);
+            Assert.Equal(0, result.TotalCount);
+        }
+        [Fact]
+        public async Task UserListQueryHandler_Succes_QueryTest()
+        {
+            _readRepoMock.Setup(p => p.ItemList(It.IsAny<ODataQueryOptions<CustomerRead>>()))
+                .Returns(Task.FromResult(new Tuple<List<CustomerRead>, int>(new List<CustomerRead>
+                {
+                    new CustomerRead(
+                                        "Mohammad",
+                                        new Domain.ValueObject.PhoneNumber("+989384563280"),
+                                        "taghy@gmail.com",
+                                        new Domain.ValueObject.BankAccountNumber("IR830120010000001387998021"),
+                                        "Yami",
+                                        DateTime.Now.AddYears(-1),
+                                        1
+                                    )
+                }, 1)));
+            QueryResponse<List<CustomerDto>> result =
+                await _handler.Handle(GetCustomerListQuery("https://localhost:8080/?$top=10&$skip=0&$orderby=Id"), CancellationToken.None);
+
+            _readRepoMock.Verify(p => p.ItemList(It.IsAny<ODataQueryOptions<CustomerRead>>()), Times.Once);
+            Assert.Equal(1, result.TotalCount);
+        }
+        private CustomerItemListQyery GetCustomerListQuery(string url)
+        {
+            return new CustomerItemListQyery(MakeOdataQueryOption(url));
+        }
+        protected ODataQueryOptions<CustomerRead> MakeOdataQueryOption(string url)
+        {
+            IEdmModel edmModel = CreateEdmModel();
+            HttpRequest request = CreateHttpRequest(url, edmModel);
+            var oDataQueryContext = new ODataQueryContext(edmModel, typeof(CustomerRead), new ODataPath());
+            return new ODataQueryOptions<CustomerRead>(oDataQueryContext, request);
+
+
+        }
+        private HttpRequest CreateHttpRequest(string url, IEdmModel edmModel)
+        {
+
+            const string routeName = "odata";
+            IEdmEntitySet entitySet = edmModel.EntityContainer.FindEntitySet(typeof(CustomerRead).Name);
+            ODataPath path = new ODataPath(new EntitySetSegment(entitySet));
+
+            var request = RequestFactory.Create("GET",
+                url,
+                dataOptions => dataOptions.AddRouteComponents(routeName, edmModel));
+
+            request.ODataFeature().Model = edmModel;
+            request.ODataFeature().Path = path;
+            request.ODataFeature().RoutePrefix = routeName;
+            return request;
+        }
+        private static IEdmModel CreateEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+
+            builder.EntitySet<CustomerRead>(typeof(CustomerRead).Name);
+            return builder.GetEdmModel();
+        }
+        public static class RequestFactory
+        {
+            /// <summary>
+            /// Creates the <see cref="HttpRequest"/> with OData configuration.
+            /// </summary>
+            /// <param name="method">The http method.</param>
+            /// <param name="uri">The http request uri.</param>
+            /// <param name="setupAction"></param>
+            /// <returns>The HttpRequest.</returns>
+            public static HttpRequest Create(string method, string uri, Action<ODataOptions> setupAction)
+            {
+                HttpContext context = new DefaultHttpContext();
+                HttpRequest request = context.Request;
+
+                IServiceCollection services = new ServiceCollection();
+                services.Configure(setupAction);
+                context.RequestServices = services.BuildServiceProvider();
+
+                request.Method = method;
+                var requestUri = new Uri(uri);
+                request.Scheme = requestUri.Scheme;
+                request.Host = requestUri.IsDefaultPort ? new HostString(requestUri.Host) : new HostString(requestUri.Host, requestUri.Port);
+                request.QueryString = new QueryString(requestUri.Query);
+                request.Path = new PathString(requestUri.AbsolutePath);
+
+                return request;
+            }
+        }
+
+
+    }
+}
---
+++ Test/Application.Test/UseCases/Customers/Query/Customers/CustomerItemListQyeryMaping_Test.cs
@@ -0,0 +1,47 @@
+﻿using Application.Test.Common;
+using Application.UseCases.Customers.Query;
+using AutoMapper;
+using Domain.ReadEntitis;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Test.UseCases.Customers.Query.Customers
+{
+    public class CustomerItemListQyeryMaping_Test : IClassFixture<MappingTestsFixture>
+    {
+        private readonly IMapper _mapper;
+
+        public CustomerItemListQyeryMaping_Test(MappingTestsFixture fixture)
+        {
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public void ShouldMap_CreateCustomerCommand_CustomerErite()
+        {
+            var entityList =
+                new List<CustomerRead> {
+                new CustomerRead(
+                                        "Mohammad",
+                                        new Domain.ValueObject.PhoneNumber("+989384563280"),
+                                        "taghy@gmail.com",
+                                        new Domain.ValueObject.BankAccountNumber("IR830120010000001387998021"),
+                                        "Yami",
+                                        DateTime.Now.AddYears(-1),
+                                        1
+                                    )
+                };
+
+            var result = _mapper.Map<List<CustomerDto>>(entityList);
+
+            Assert.NotNull(result);
+            result.ShouldBeOfType<List<CustomerDto>>();
+        }
+
+
+    }
+}
